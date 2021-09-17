5487b0cd513db23ae9d4c94079cf06d0fb37d0e6
Calvin Smith <email@cjsmith.io>
2021-09-16 22:03:10 -0500

    fixing yarn construction

--- minotaur/yarn/yarn.py
+++ minotaur/yarn/yarn.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Dict, Any
+from typing import List, Optional, Any, Dict
 
 from ..hash import hash
 from ..timer import current_time
@@ -9,6 +9,17 @@ class Value:
     name : str
     value : Any
 
+    # IO
+
+    @classmethod
+    def load(cls, json) -> "Value":
+        """Construct a Value from a JSON encoding."""
+
+        name = json["name"]
+        value = json["value"]
+
+        return cls(name=name, value=value)
+
     def dump(self):
         """Convert the instance to a JSON encoding."""
 
@@ -16,7 +27,6 @@ class Value:
             "name" : self.name,
             "value" : self.value
         }
-
 
 @dataclass
 class Identifier:
@@ -34,6 +44,8 @@ class Identifier:
             self.key = key
         else:
             self.key = hash()
+
+    # IO
 
     @classmethod
     def load(cls, json) -> "Identifier":
@@ -55,7 +67,46 @@ class Identifier:
 @dataclass
 class Yarn:
     identifier : Identifier
-    values : Mapping[str, Any]
+    values : Dict[str, Any]
     contexts : List["Yarn"]
     enter_time : int
     exit_time : int
+
+    # Accessing Attributes
+
+    @property
+    def duration(self) -> int:
+        """Total time the context was open."""
+
+        return self.exit_time - self.enter_time
+
+    # IO
+
+    @classmethod
+    def load(cls, json) -> "Yarn":
+        """Construct a Yarn object from a JSON encoding."""
+
+        identifier = Identifier.load(json["identifier"])
+        values = [Value.load(value) for value in json["values"]]
+        contexts = [cls.load(ctx) for ctx in json["contexts"]]
+        enter_time = json["enter_time"]
+        exit_time = json["exit_time"]
+
+        return cls(
+            identifier=identifier,
+            values=values,
+            contexts=contexts,
+            enter_time=enter_time,
+            exit_time=exit_time
+        )
+
+    def dump(self):
+        """Construct a JSON encoding for a Yarn object."""
+
+        return {
+            "identifier" : self.identifier.dump(),
+            "values" : [value.dump() for value in self.values],
+            "contexts" : [ctx.dump() for ctx in self.contexts],
+            "enter_time" : self.enter_time,
+            "exit_time" : self.exit_time
+        }
